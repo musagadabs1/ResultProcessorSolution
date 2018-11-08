1fcfef17b45aa52e9d533a5b929000791575e7e5
Musa Sule <musagadabs1@.com>
2018-11-08 15:39:33 +0100

    AdminMenuAdded

---
+++ ResultProcessor/Models/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResultProcessor.Models
+{
+    public static class Constants
+    {
+        public const string Administrator = "Admin";
+        public const string Manager = "Manager";
+        public const string User = "User";
+    }
+}
